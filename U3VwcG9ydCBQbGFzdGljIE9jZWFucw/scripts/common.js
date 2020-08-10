857873315eb0a9911ae23787cdf409333f014769
shaunakg <gadkari.shaunak@gmail.com>
2020-08-10 11:34:32 +1000

    made frontpage

---
+++ U3VwcG9ydCBQbGFzdGljIE9jZWFucw/scripts/common.js
@@ -0,0 +1,10 @@
+
+console_styling = `
+font-weight: bold;
+background-color: lightblue;
+color: black;
+font-size: 1rem;
+font-family: Helvetica, sans-serif
+`;
+
+console.log("%c Welcome to JMSS Plastic Oceans :) ", console_styling)
