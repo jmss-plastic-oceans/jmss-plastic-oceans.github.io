8166e630078370707e2e769d4c226c90148ed50d
shaunakg <gadkari.shaunak@gmail.com>
2020-08-10 13:17:12 +1000

    puzzle !!!!

---
+++ puzzle.js
@@ -0,0 +1,20 @@
+let p = `VmpGYVYySXhWWGROVldoVllUSjRWbFpy
+V25kVWJIQlhWVzVPYkZKdGVIcFpWVlUx
+WWtkRmVtRkdjRlpXZWxaUVZqSjRhMUpy
+TlVsYVJuQlhZbFpLVUZkclZtdFRiVlpY
+Vlc1U2JGSnVRbGhhVjNoYVRXeGFSbGt6
+WkU1V01IQkpWbGQwYjJKR1RrWlRiV2hh
+WWxSRmQxUldXbXRXTWtaR1ZHMTBUbE5I
+VVRKV2JYaHZaREpGZVZOdVVsWmlhMXBa
+V1cwMVEyVnNVbk5YYm1SVVVteEtNRmxy
+WkhkV01ERkpVV3BHVjAxcVFYaGFSRXBH
+WlVkSmVtSkhlRlJTVm5CMlZtMXdSMVpy
+TVZkaVJtaFBWMGRTVDFSV2FFTk5WbHAw
+VFZoa2FFMXNXbFpYYm5CeVVGRTlQUT09`;
+
+console.log('%c' + p, "color: white; background-color: coral");
+
+document.querySelectorAll("img")[0].onclick = function (e) {
+    this.title = "There's a message hidden on this website. Decode it to find the location of our preview site.";
+    this.dataset.click = "true"
+}
