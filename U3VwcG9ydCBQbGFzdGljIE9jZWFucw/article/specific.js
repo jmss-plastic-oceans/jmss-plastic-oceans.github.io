db441e5ed9eb1940e7432004c763d274d6ee3cce
shaunakg <gadkari.shaunak@gmail.com>
2020-08-21 10:30:55 +1000

    shortlink integration

--- U3VwcG9ydCBQbGFzdGljIE9jZWFucw/article/specific.js
+++ U3VwcG9ydCBQbGFzdGljIE9jZWFucw/article/specific.js
@@ -2,6 +2,26 @@
 const urlParams = new URLSearchParams(document.location.search);
 const loading = document.getElementById("loading");
 const textWrapper = document.getElementById("text");
+const share = document.getElementById("share");
+const oldURL = document.location.href;
+let shortUrl, articleTitle;
+
+share.onclick = () => {
+    if (shortUrl) {
+        navigator.share({
+            title: articleTitle,
+            text: "An article by Plastic Oceans Students",
+            url: shortUrl
+        })
+    } else {
+        share.style.backgroundColor = "lightcoral";
+        share.style.color = "white;"
+        setTimeout(() => {
+            share.style.backgroundColor = "unset"
+            share.style.color = "black";
+        }, 1000);
+    }
+}
 
 // The CORS proxy circumvents the Access-Control-Allow-Origin header on the Google servers.
 const CORS = "https://srg-cors-proxy.herokuapp.com/";
@@ -26,15 +46,16 @@ function string_to_slug (str) {
 }
 
 if (!urlParams.get("data")) {
+
     loading.innerHTML = "Sorry! Article not found. <a href='../'>Go back</a>";
+
 } else {
 
     const { url, title } = JSON.parse(atob(urlParams.get("data")));
+    articleTitle = title;
 
     fetch(CORS + url).then((response) => {
         response.text().then((text) => {
-
-            history.replaceState(JSON.parse(atob(urlParams.get("data"))), `${title} | Plastic Oceans @ John Monash Science School`, string_to_slug(title));
 
             let newP = document.createElement("p")
             newP.innerText = text;
@@ -47,6 +68,25 @@ if (!urlParams.get("data")) {
             textWrapper.appendChild(newP);
 
         })
+    });
+
+    const linkData = {
+        destination: oldURL,
+        domain: { fullName: "rebrand.ly" },
+        title: `Article by POA Students - ${title}`
+    };
+
+    fetch('https://api.rebrandly.com/v1/links', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            "apikey": "51a53394d56b49ba87451c4fc42810b7"
+        },
+        body: JSON.stringify(linkData)
+    }).then(res=>res.json()).then(res => {
+        console.log(res);
+        shortUrl = link.shortUrl;
     });
 
 
