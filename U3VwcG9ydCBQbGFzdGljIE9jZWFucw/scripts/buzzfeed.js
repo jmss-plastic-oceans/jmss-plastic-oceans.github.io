4655f66ab38a710129d55649aa1ea7eab770b943
shaunakg <gadkari.shaunak@gmail.com>
2020-08-22 18:01:41 +1000

    fixed quizzes (better than last time)

--- U3VwcG9ydCBQbGFzdGljIE9jZWFucw/scripts/buzzfeed.js
+++ U3VwcG9ydCBQbGFzdGljIE9jZWFucw/scripts/buzzfeed.js
@@ -25,7 +25,7 @@ function buzzfeedSetup() {
             link.href = "https://buzzfeed.com" + b.canonical_path;
 
             let img = document.createElement("img");
-            img.src = b.images.standard;
+            img.src = b.images.big;
             
             link.appendChild(img);
             newQuizElement.appendChild(link);
